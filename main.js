2e5852db60171ac603d92647ed0cea448787bac6
Roger Chung <84298258+rch-goldsnaker@users.noreply.github.com>
2022-08-20 21:03:16 -0300

    Adding option to choose camara

--- main.js
+++ main.js
@@ -5,6 +5,54 @@ const classifier = knnClassifier.create();
 const classes = ["Untrained","","",""];
 const numberTrainings = ["","",""]
 
+
+const select = document.getElementById('select');
+
+function gotDevices(mediaDevices) {
+    select.innerHTML = '';
+    select.appendChild(document.createElement('option'));
+    let count = 1;
+    mediaDevices.forEach(mediaDevice => {
+      if (mediaDevice.kind === 'videoinput') {
+        const option = document.createElement('option');
+        option.value = mediaDevice.deviceId;
+        const label = mediaDevice.label || `Camera ${count++}`;
+        const textNode = document.createTextNode(label);
+        option.appendChild(textNode);
+        select.appendChild(option);
+      }
+    });
+}
+
+button.addEventListener('click', event => {
+    if (typeof currentStream !== 'undefined') {
+      stopMediaTracks(currentStream);
+    }
+    const videoConstraints = {};
+    if (select.value === '') {
+      videoConstraints.facingMode = 'environment';
+    } else {
+      videoConstraints.deviceId = { exact: select.value };
+    }
+    const constraints = {
+      video: videoConstraints,
+      audio: false
+    };
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(stream => {
+        currentStream = stream;
+        video.srcObject = stream;
+        return navigator.mediaDevices.enumerateDevices();
+      })
+      .then(gotDevices)
+      .catch(error => {
+        console.error(error);
+      });
+});
+
+navigator.mediaDevices.enumerateDevices().then(gotDevices);
+
 async function app(){
 
     net = await mobilenet.load();
